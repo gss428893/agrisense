d7fb79a85ec70e6d41c3bcb17f0bd2680f0c9c83
gss428893 <gss428893@gmail.com>
2026-08-30 09:35:33 +0530

    Update 2_app_final.py

--- 2_app_final.py
+++ 2_app_final.py
@@ -220,7 +220,7 @@ DISEASE_DB = {
 # ---------- Load Model ----------
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model("crop_disease_model_fixed.keras")
+ model = tf.keras.models.load_model("crop_disease_model_fixed.keras", compile=False)
     with open("class_indices.json", "r") as f:
         class_indices = json.load(f)
     idx_to_class = {v: k for k, v in class_indices.items()}
